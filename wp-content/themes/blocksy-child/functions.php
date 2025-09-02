c963de8be1904359da5b3fcf87e507f16503146d
Fei0716 <ahfeiahfeiahfei@gmail.com>
2025-09-03 00:56:16 +0800

    Modify the page's color scheme for more vibrancy

--- wp-content/themes/blocksy-child/functions.php
+++ wp-content/themes/blocksy-child/functions.php
@@ -7,3 +7,5 @@ if (! defined('WP_DEBUG')) {
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 });
+//to disable auto image rescaled to 2560px
+add_filter( 'big_image_size_threshold', '__return_false' );
